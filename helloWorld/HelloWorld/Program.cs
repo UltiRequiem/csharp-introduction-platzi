4557fae970bc09ab02abf3dc225e5990a85eaf4d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-12-16 07:55:18 -0500

    :tada: Begin

---
+++ helloWorld/HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
